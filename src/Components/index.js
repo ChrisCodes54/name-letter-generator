9b37da32549fdea825b5278a56f1d90f54207c6d
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-09-19 15:43:42 -0400

    centered text and button in container

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,14 +3,16 @@ import './style.css'
 
 export default function NameInputForm() {
   return (
-    <div className="input-container border border-3 border-danger rounded p-3">
-			<div className="mb-3">Enter your name below!</div>
+    <div className="input-container border border-3 border-danger rounded p-3 ">
+			<div className="mb-3 d-flex justify-content-center">Enter your name below!</div>
 			<div class="input-group mb-3">
 				<input type="text" class="form-control" placeholder="Name" />
 			</div>
-			<button type="button" class="btn btn-danger">
-				Primary
+			<div className='d-flex justify-content-center'>
+			<button type="button" className="btn btn-danger">
+				Submit
 			</button>
+		</div>
 		</div>
   )
 }
