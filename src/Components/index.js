3bf6ee905574f4c20450686dab772641bacd8141
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-09-19 15:47:58 -0400

    centered input as well to keep everything uniform

--- src/Components/index.js
+++ src/Components/index.js
@@ -6,7 +6,7 @@ export default function NameInputForm() {
     <div className="input-container border border-3 border-danger rounded p-3 ">
 			<div className="mb-3 d-flex justify-content-center">Enter your name below!</div>
 			<div class="input-group mb-3">
-				<input type="text" class="form-control" placeholder="Name" />
+				<input type="text" class="form-control text-center" placeholder="Name" />
 			</div>
 			<div className='d-flex justify-content-center'>
 			<button type="button" className="btn btn-danger">
