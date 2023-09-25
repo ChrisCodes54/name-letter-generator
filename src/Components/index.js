02742a28203d0aedbb5ec91dc073c8d96407a41d
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-09-25 14:56:25 -0400

    started to test how to grab input text from react component when user submits their name

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './style.css'
 
+
 export default function NameInputForm() {
+	const handleClick = () => {
+
+	}
+	
   return (
     <div className="input-container border border-3 border-danger rounded p-3 ">
 			<div className="mb-3 d-flex justify-content-center">Enter your name below!</div>
 			<div class="input-group mb-3">
-				<input type="text" class="form-control text-center" placeholder="Name" />
+				<input type="text" className="form-control text-center" placeholder="Name" id='name-input' onClick={()=> handleClick}  />
 			</div>
 			<div className='d-flex justify-content-center'>
 			<button type="button" className="btn btn-danger">
