b9b3c92470923e4f28a08db2b4f985cb3bbe0611
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-10-03 16:15:18 -0400

    set up functions to grab input, tested it out through the console log and seems to be coming through. next will need to work on displaying to the user

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'
 
 
 export default function NameInputForm() {
+	const[name, setName] = useState('')
+
+
 	const handleClick = () => {
+		console.log(name.charAt(Math.floor(Math.random() * name.length)))
 
 	}
+	const handleInputChange = (event) => {
+		setName(event.target.value)
+	}
+
+	
 	
   return (
     <div className="input-container border border-3 border-danger rounded p-3 ">
 			<div className="mb-3 d-flex justify-content-center">Enter your name below!</div>
-			<div class="input-group mb-3">
-				<input type="text" className="form-control text-center" placeholder="Name" id='name-input' onClick={()=> handleClick}  />
+			<div className="input-group mb-3">
+				<input type="text" className="form-control text-center" placeholder="Name" id='name-input' value={name} onChange={handleInputChange} />
 			</div>
 			<div className='d-flex justify-content-center'>
-			<button type="button" className="btn btn-danger" onClick={()=> handleClick} >
+			<button type="button" className="btn btn-danger" onClick={handleClick} >
 				Submit
 			</button>
 		</div>
