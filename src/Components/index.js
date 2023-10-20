5105a2d2eab41a0d83e6cec72a094c9cfa69a0bc
Christopher Tangarife <christophertangarife@christophersmbp.lan>
2023-10-20 10:20:52 -0400

    created function that does not allow the user to type in any special characters on change

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default function NameInputForm() {
   const [name, setName] = useState("");
   const [randomLetter, setRandomLetter] = useState("");
-const nameRegex = /^[a-zA-Z\s]+$/;
+  const nameRegex = /^[a-zA-Z\s]+$/;
 
   const handleClick = () => {
     const trimmedName = name.trim();
@@ -20,38 +20,43 @@ const nameRegex = /^[a-zA-Z\s]+$/;
     }
   };
   const handleInputChange = (event) => {
-    setName(event.target.value);
+    const inputValue = event.target.value.replace(/[^a-zA-Z\s]+/g, "")
+    setName(inputValue);
   };
 
+  
+  
   return (
     <>
-    <div className="input-container border border-3 border-danger rounded p-3 ">
-    { randomLetter && (
-        <div className="mb-3 d-flex justify-content-center">
-          Random Letter: {randomLetter}
-
+      <div className="input-container border border-3 border-danger rounded p-3 ">
+        {randomLetter && (
+          <div className="mb-3 d-flex justify-content-center">
+            Random Letter: {randomLetter}
           </div>
-      )}
-      <div className="mb-3 d-flex justify-content-center">
-        Enter your name below!
+        )}
+        <div className="mb-3 d-flex justify-content-center">
+          Enter your name below!
+        </div>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control text-center"
+            placeholder="Name"
+            id="name-input"
+            value={name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="d-flex justify-content-center">
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={handleClick}
+          >
+            Submit
+          </button>
+        </div>
       </div>
-      <div className="input-group mb-3">
-        <input
-          type="text"
-          className="form-control text-center"
-          placeholder="Name"
-          id="name-input"
-          value={name}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div className="d-flex justify-content-center">
-        <button type="button" className="btn btn-danger" onClick={handleClick}>
-          Submit
-        </button>
-      </div>
-      
-    </div>
     </>
   );
 }
